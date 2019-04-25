3b604ec1f11677dba1a935bb9525295c45af99cc
Danil Buhokov <tradinal@gmail.com>
2019-04-25 11:14:19 +0300

    Init commit

---
+++ app/src/main/java/com/skillup/bigdig/serviceproject/MainActivity.java
@@ -0,0 +1,73 @@
+package com.skillup.bigdig.serviceproject;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+public class MainActivity extends AppCompatActivity {
+
+    Unbinder unbinder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        unbinder = ButterKnife.bind(this);
+        configureBroadcastReceiver();
+    }
+
+    @OnClick(R.id.bt_start_service)
+    void btStartService(){
+        Intent intent = new Intent(this, MyService.class);
+        startService(intent);
+        sendLocalBroadcast(StartUpBroadcastReceiver.START_ACTION);
+    }
+
+    @OnClick(R.id.bt_start_task)
+    void btStartTask(){
+        Intent intent = new Intent(this, MyService.class);
+        intent.setAction(MyService.ACTION_START_TASK);
+        startService(intent);
+        sendLocalBroadcast(StartUpBroadcastReceiver.START_TASK_ACTION);
+    }
+
+    @OnClick(R.id.bt_pause_task)
+    void btPauseTask(){
+        Intent intent = new Intent(this, MyService.class);
+        intent.setAction(MyService.ACTION_STOP_TASK);
+        startService(intent);
+        sendLocalBroadcast(StartUpBroadcastReceiver.STOP_TASK_ACTION);
+    }
+
+    @OnClick(R.id.bt_stop_service)
+    void btStopService(){
+        Intent intent = new Intent(this, MyService.class);
+        stopService(intent);
+        sendLocalBroadcast(StartUpBroadcastReceiver.STOP_ACTION);
+    }
+
+    @Override
+    protected void onDestroy() {
+        unbinder.unbind();
+        super.onDestroy();
+    }
+
+    private void sendLocalBroadcast(String action){
+        Intent broadcastIntent = new Intent(action);
+        sendBroadcast(broadcastIntent);
+    }
+
+    private void configureBroadcastReceiver(){
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(StartUpBroadcastReceiver.START_ACTION);
+        intentFilter.addAction(StartUpBroadcastReceiver.STOP_ACTION);
+        intentFilter.addAction(StartUpBroadcastReceiver.START_TASK_ACTION);
+        intentFilter.addAction(StartUpBroadcastReceiver.STOP_TASK_ACTION);
+        registerReceiver(new StartUpBroadcastReceiver(), intentFilter);
+    }
+}
